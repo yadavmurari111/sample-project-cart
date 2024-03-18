976e9aedee379d653cbb4659aa7450242372ee3c
Murari Yadav <murari.yadav@thingmzing.com>
2024-03-18 16:11:34 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,26 @@
+export enum routes{
+    SIGN_IN_SCREEN='SIGN_IN_SCREEN',
+    MY_ACCOUNT='MY_ACCOUNT',
+    ROOMS='ROOMS',
+    CONTACTS='CONTACTS',
+    HOME='HOME',
+    ROOM_CHAT = 'ROOM_CHAT',
+    SIGN_UP_SCREEN="signup"
+}
+export const colors = {
+    black:'#000000',
+    white:'#ffffff',
+    primary:'#eeaaf5',
+    secondary:'#010101',
+    contentBG:'#101418',
+}
+export const icons = {
+    contacts:require('../assets/images/contacts.png'),
+    profile:require('../assets/images/profile.png'),
+    back:require('../assets/images/back.png'),
+    more:require('../assets/images/more.png'),
+    flash:require('../assets/images/flash.png'),
+    mic:require('../assets/images/mic.png'),
+    send:require('../assets/images/send.png'),
+    smile:require('../assets/images/smile.png'),
+}
